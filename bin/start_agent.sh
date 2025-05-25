81ceb4cc4bc5587db69929469cb2839ba9b746a0
takutaka <takutaka@pepabo.com>
2025-05-25 22:17:26 +0900

    fix start_agent.sh

--- bin/start_agent.sh
+++ bin/start_agent.sh
@@ -2,4 +2,4 @@
 PROMPT="$SLACK_AGENT_PROMPT"
 mkdir -p sessions/$SLACK_THREAD_TS
 cd sessions/$SLACK_THREAD_TS
-claude -c --output-format stream-json -p --verbose  "$PROMPT" | claude-posts --bot-token=$SLACK_BOT_TOKEN --channel-id=$SLACK_CHANNEL_ID --thread-ts=$SLACK_THREAD_TS &
+mise exec -- claude -c --output-format stream-json -p --verbose  "$PROMPT" | claude-posts --bot-token=$SLACK_BOT_TOKEN --channel-id=$SLACK_CHANNEL_ID --thread-ts=$SLACK_THREAD_TS &
