002e74cc6b2ccf8d1c73245bac5b1471a2ec8431
takutaka <takutaka@pepabo.com>
2025-05-25 13:30:56 +0900

    離す

--- bin/start_agent.sh
+++ bin/start_agent.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 PROMPT=$SLACK_AGENT_PROMPT
-mise exec -- claude --output-format stream-json -p --verbose "$PROMPT" | claude-posts --bot-token=$SLACK_BOT_TOKEN --channel-id=$SLACK_CHANNEL_ID --thread-ts=$SLACK_THREAD_TS
+mkdir -p sessions/$SLACK_THREAD_TS
+cd sessions/$SLACK_THREAD_TS
+claude -c --output-format stream-json -p --verbose  \'$PROMPT\' | claude-posts --bot-token=$SLACK_BOT_TOKEN --channel-id=$SLACK_CHANNEL_ID --thread-ts=$SLACK_THREAD_TS &
